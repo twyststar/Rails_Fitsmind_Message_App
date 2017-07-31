a839b9638bc328945a958608cfde597a683370f5
Starlynne <lewis.start0905@gmail.com>
2017-07-31 10:13:57 -0700

    Add user views, add admin to user, add routes for home, user, admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
+  root to: "home#welcome"
+
+  devise_for :users
+
+  resources :home, :only => [:welcome, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
